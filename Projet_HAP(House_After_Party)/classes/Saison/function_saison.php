abd4a0ff917c20d0d66d90d14fda83e166d44041
Omnous-Luminae <enjolras.ethan3@gmail.com>
2025-10-02 09:34:48 +0200

    Add files via upload

---
+++ Projet_HAP(House_After_Party)/classes/Saison/function_saison.php
@@ -0,0 +1,26 @@
+<?php
+
+Class Saison {
+
+    private $idSaison;
+    private $lib_Saison;
+
+    public function __construct($idSaison, $lib_Saison) {
+        $this->idSaison = $idSaison;
+        $this->lib_Saison = $lib_Saison;
+    }
+
+    public function getIdSaison() {
+        return $this->idSaison;
+    }
+
+    public function getlibSaison() {
+        return $this->lib_Saison;
+    }
+    public function setlibSaison($lib_Saison) {
+        $this->lib_Saison = $lib_Saison;
+    }
+
+}
+
+?>
